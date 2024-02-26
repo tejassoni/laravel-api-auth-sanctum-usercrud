14d941dd18468d84a7371a653d22cb42b1e47956
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-02-26 21:20:07 +0530

    Sanctum auth controller added

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ https://laravel.com/docs/10.x/sanctum
 # References
 <ol>
  <li>https://laravel.com/docs/10.x/sanctum#installation</li> 
+ <li>https://demos.pixinvent.com/vuexy-vuejs-admin-template/documentation/guide/laravel-integration/laravel-sanctum-authentication.html</li>
+ <li>https://www.webappfix.com/post/laravel-10-sanctum-api-authentication-tutorial-example.html</li>
 <ol> 
 
 # Search Key
---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+     /**
+    * Create user
+    *
+    * @param  [string] name
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [string] password_confirmation
+    * @return [string] message
+    */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email'=>'required|string|unique:users',
+            'password'=>'required|string',
+            'c_password' => 'required|same:password'
+        ]);
+
+        $user = new User([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($user->save()){
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+            'message' => 'Successfully created user!',
+            'accessToken'=> $token,
+            ],201);
+        }
+        else{
+            return response()->json(['error'=>'Provide proper details']);
+        }
+    }
+}
